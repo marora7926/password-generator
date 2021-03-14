e59b23d0a3d4726e5a1105bc182c9b3bd5581018
Mohit Arora <mohit.arora@sydney.edu.au>
2021-03-14 14:57:05 +1100

    updated readme and js file

--- script.js
+++ script.js
@@ -1,37 +1,55 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//define minimum and maximum length
+goodlength: function(){
+  if (this.length <8 && this.lenght>128){
+    return false;
+    }
+  else {
+    return true;
+    } 
+}
+
+//define arrays
+var lowercase = ['a', 'b', 'c', 'd',  'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+var numeric = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+//special charaters from OWASP website
+var special = ['!', '"', '#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '_', '{', '|', '}', '~']
+
+//Prompt to choose ("prompt" command) the length for a password between 8 and 128 characters inclusive.
+passwordLength: function() {
+  this.length = prompt ("Your password length should be between 8 and 128 charaters inclusive");
+}
+
+//Prompt to choos ("confirm" command) lowercase charater/s for a password.
+passwordLower: function(){
+  this.lower = confirm ("Do you wish to have lowercase character/s in your password"); 
+}
+
+//Prompt to choose ("confirm" command) uppercase charater/s for a password.
+passwordUpper: function(){
+  this.upper = confirm ("Do you wish to have uppercase character/s in your password"); 
+}
+
+//Prompt to choose ("confirm" command) numeric charater/s for a password.
+passwordNumber: function(){
+  this.number = confirm ("Do you wish to have muneric character/s in your password"); 
+}
+
+//Prompt to choose ("confirm" command) special charater/s for a password.
+passwordSpecial: function(){
+  this.special = confirm ("Do you wish to have special character/s in your password"); 
+}
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-}
-
-//Prompting for length for a password between 8 and 128 characters inclusive.
-passwordLength: function() {
-  this.length = prompt ("Your password length should be between 8 and 128 charaters inclusive");
-}
-
-//Prompting for inclusion of a lowercase charater/s for a password.
-passwordLower: function(){
-  this.lower = confirm ("Your password should have lowercase character/s"); 
-}
-
-//Prompting for inclusion of an uppercase charater/s for a password.
-passwordUpper: function(){
-  this.upper = confirm ("Your password should have uppercase character/s"); 
-}
-
-//Prompting for inclusion of a numeric charater/s for a password.
-passwordNumber: function(){
-  this.number = confirm ("Your password should have numberic character/s"); 
-}
-
-//Prompting for inclusion of special charater/s for a password.
-passwordSpecial: function(){
-  this.special = confirm ("Your password should have an uppercase character"); 
 }
 
 // Add event listener to generate button
