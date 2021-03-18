d1313e31527a91f7bf31af0772b7c8ef9bd55164
Mohit Arora <mohit.arora@sydney.edu.au>
2021-03-18 14:00:22 +1100

    updates

--- script.js
+++ script.js
@@ -2,15 +2,28 @@
 var generateBtn = document.querySelector("#generate");
 
 //define arrays
-allCharacters = [],
-  lowerCh = ['a', 'b', 'c', 'd',  'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
-  upperCh = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
-  numericCh = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
-//special charaters from OWASP website
-  specialCh = ['!', '"', '#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '_', '{', '|', '}', '~'],
-console.log(allCharaters)
+var newPwd = {
+  lowerCh1: ['a', 'b', 'c', 'd',  'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
+  upperCh1: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
+  numericCh1: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
+  //special charaters from OWASP website
+  specialCh1: ['!', '"', '#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '_', '{', '|', '}', '~'],
+}
+//verify the above arrays - self-check #1
+console.log(newPwd)
 
-//Prompt and verification to choose ("prompt" command) the length for a password between 8 and 128 characters inclusive.
+
+// defining the properties of the current (baseline definition) password
+var curPwd = {
+  pwd0: "",
+  length: 0,
+  lowerCh0: true,
+  upperCh0: true,
+  numericCh0: true,
+  specialCh0: true,
+}
+
+// // Prompt and verification to choose ("prompt" command) the length for a password between 8 and 128 characters inclusive.
 // passwordLength: function() {
 //   this.length = window.prompt("write a number between 8 and 128 charaters inclusive for your password length");
 //   if (this.length <8 || this.length>128){
@@ -68,13 +81,15 @@ console.log(allCharaters)
 //   this.special = confirm ("Do you wish to have special character/s in your password"); 
 // }
 
+
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
